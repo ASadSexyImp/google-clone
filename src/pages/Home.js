3017db7a5417b96f39d6088026fee59022831a9d
ASadSexyImp <masakazuplus@gmail.com>
2022-08-03 19:11:06 +0900

    dependency solved with yarn not npm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,19 @@ import './Home.css';
 
 function Home() {
     return (
+        <div className="home">
+            <div className="header">
+                <div className="headerLeft">
 
+                </div>
+                <div className="headerRight">
+            
+                </div>
+            </div>
+            <div className="home_body">
+            
+            </div>
+        </div>
     )
 }
 
